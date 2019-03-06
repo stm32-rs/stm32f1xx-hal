a5b873735505d4a9ec8bd3dc4d650cca0579945d
TheZoq2 <frans.skarman@gmail.com>
2019-03-06 22:11:00 +0100

    Improve comments in blinky example

--- examples/blinky.rs
+++ examples/blinky.rs
@@ -47,12 +47,14 @@ fn main() -> ! {
     // Acquire the GPIOC peripheral
     let mut gpioc = dp.GPIOC.split(&mut rcc.apb2);
 
-    // Configure gpio C pin 13 as a push-pull output
+    // Configure gpio C pin 13 as a push-pull output. The `crh` register is passed to the function
+    // in order to configure the port. For pins 0-7, crl should be passed instead.
     let mut led = gpioc.pc13.into_push_pull_output(&mut gpioc.crh);
     // Configure the syst timer to trigger an update every second
     let mut timer = Timer::syst(cp.SYST, 1.hz(), clocks);
+
+    // Wait for the timer to trigger an update and change the state of the LED
     loop {
-        // Wait until the timer triggers an update
         block!(timer.wait()).unwrap();
         led.set_high();
         block!(timer.wait()).unwrap();
