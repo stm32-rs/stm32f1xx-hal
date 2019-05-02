24ba6873368c42538123b84cb55b09f3604fe5b2
Paul Florence <perso@florencepaul.com>
2019-05-02 16:08:20 +0200

    Reexport cortex-m

--- src/prelude.rs
+++ src/prelude.rs
@@ -12,3 +12,4 @@ pub use crate::rcc::RccExt as _stm32_hal_rcc_RccExt;
 //pub use crate::serial::ReadDma as _stm32_hal_serial_ReadDma;
 //pub use crate::serial::WriteDma as _stm32_hal_serial_WriteDma;
 pub use crate::time::U32Ext as _stm32_hal_time_U32Ext;
+pub use cortex_m;
