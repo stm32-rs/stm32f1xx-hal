ca26026b889d62f0ff451c7f870cfda2fb06fc94
Johnny Egeland <johnnyege@gmail.com>
2020-12-04 22:47:18 +0100

    Re-instated the old simple SPI DMA example, and added a new example for use via abstraction.

--- examples/spi-dma.rs
+++ examples/spi-dma.rs
@@ -6,17 +6,13 @@
 */
 use panic_halt as _;
 
-use cortex_m::singleton;
 use cortex_m_rt::entry;
 use stm32f1xx_hal::{
-    dma::{Transfer, Transferable, WriteDma, R},
     pac,
     prelude::*,
-    spi::{Mode, Phase, Polarity, Spi, SpiTxDma},
+    spi::{Mode, Phase, Polarity, Spi},
+    dma::Transferable
 };
-
-// Set size of data buffer to transmit via SPI DMA
-const DATA_BUFFER_SIZE: usize = 8;
 
 #[entry]
 fn main() -> ! {
@@ -51,51 +47,14 @@ fn main() -> ! {
     let dma = dp.DMA1.split(&mut rcc.ahb);
 
     // Connect the SPI device to the DMA
-    let mut spi_dma = spi.with_tx_dma(dma.5);
+    let spi_dma = spi.with_tx_dma(dma.5);
 
-    // Create a mutable data buffer, as we need to write data into it
-    let mut data_buffer: &'static mut [u8] =
-        singleton!(: [u8; DATA_BUFFER_SIZE] = [0; DATA_BUFFER_SIZE]).unwrap();
+    // Start a DMA transfer
+    let transfer = spi_dma.write(b"hello, world");
 
-    // Use a byte counter to generate some data for our buffer
-    let mut data_byte = 0u8;
+    // Wait for it to finnish. The transfer takes ownership over the SPI device
+    // and the data being sent anb those things are returned by transfer.wait
+    let (_buffer, _spi_dma) = transfer.wait();
 
-    // Do SPI transmission in an endless loop
-    loop {
-        // Fill the buffer with some data
-        for ix in 0..DATA_BUFFER_SIZE {
-            // Increase by 1, and insure it wraps
-            data_byte = data_byte.wrapping_add(1u8);
-
-            // Put the byte into the buffer
-            data_buffer[ix] = data_byte;
-        }
-
-        // Call write function
-        let transfer = write_spi_dma(spi_dma, data_buffer);
-
-        // Wait for transfer to complete
-        let (ret_buffer, ret_spidma) = transfer.wait();
-
-        // Return ownership, so we can re-use these the next iteration
-        data_buffer = ret_buffer;
-        spi_dma = ret_spidma;
-    }
-}
-
-/// The writing is done is a separate function, as this is typically how a driver would work
-/// The Driver will take ownership of the SPI DMA for the duration of the Transfer Operation.
-/// When complete, the wait() function returns the ownership of the buffer and SPI DMA, which
-/// makes it usabe for something else. This way, one SPI can be shared among multiple drivers.
-fn write_spi_dma<SPI, REMAP, PINS, CHANNEL>(
-    spi_dma: SpiTxDma<SPI, REMAP, PINS, CHANNEL>,
-    buffer: &'static mut [u8],
-) -> Transfer<R, &'static mut [u8], SpiTxDma<SPI, REMAP, PINS, CHANNEL>>
-where
-    SpiTxDma<SPI, REMAP, PINS, CHANNEL>: WriteDma<&'static mut [u8], u8>,
-    Transfer<R, &'static mut [u8], SpiTxDma<SPI, REMAP, PINS, CHANNEL>>:
-        Transferable<&'static mut [u8], SpiTxDma<SPI, REMAP, PINS, CHANNEL>>,
-{
-    // Simply call write, and return the Transfer instance
-    spi_dma.write(buffer)
+    loop {}
 }
