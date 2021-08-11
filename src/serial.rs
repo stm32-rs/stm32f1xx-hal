01dfad32c209a10c3f017d416d34342064c4bddf
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2021-08-11 09:38:05 +0300

    clear_idle_interrupt

--- src/serial.rs
+++ src/serial.rs
@@ -294,19 +294,27 @@ where
         }
     }
 
-    /// Return true if the line idle status is set
+    /// Returns true if the line idle status is set
     pub fn is_idle(&self) -> bool {
         self.usart.sr.read().idle().bit_is_set()
     }
 
-    /// Return true if the tx register is empty (and can accept data)
+    /// Returns true if the tx register is empty (and can accept data)
     pub fn is_tx_empty(&self) -> bool {
         self.usart.sr.read().txe().bit_is_set()
     }
 
-    /// Return true if the rx register is not empty (and can be read)
+    /// Returns true if the rx register is not empty (and can be read)
     pub fn is_rx_not_empty(&self) -> bool {
         self.usart.sr.read().rxne().bit_is_set()
+    }
+
+    /// Clear idle line interrupt flag
+    pub fn clear_idle_interrupt(&self) {
+        unsafe {
+            let _ = (*USART::ptr()).sr.read();
+            let _ = (*USART::ptr()).dr.read();
+        }
     }
 
     /// Returns ownership of the borrowed register handles
@@ -383,14 +391,17 @@ impl<USART> Tx<USART>
 where
     USART: Instance,
 {
+    /// Start listening for transmit interrupt event
     pub fn listen(&mut self) {
         unsafe { (*USART::ptr()).cr1.modify(|_, w| w.txeie().set_bit()) };
     }
 
+    /// Stop listening for transmit interrupt event
     pub fn unlisten(&mut self) {
         unsafe { (*USART::ptr()).cr1.modify(|_, w| w.txeie().clear_bit()) };
     }
 
+    /// Returns true if the tx register is empty (and can accept data)
     pub fn is_tx_empty(&self) -> bool {
         unsafe { (*USART::ptr()).sr.read().txe().bit_is_set() }
     }
@@ -400,28 +411,42 @@ impl<USART> Rx<USART>
 where
     USART: Instance,
 {
+    /// Start listening for receive interrupt event
     pub fn listen(&mut self) {
         unsafe { (*USART::ptr()).cr1.modify(|_, w| w.rxneie().set_bit()) };
     }
 
+    /// Stop listening for receive interrupt event
     pub fn unlisten(&mut self) {
         unsafe { (*USART::ptr()).cr1.modify(|_, w| w.rxneie().clear_bit()) };
     }
 
+    /// Start listening for idle interrupt event
     pub fn listen_idle(&mut self) {
         unsafe { (*USART::ptr()).cr1.modify(|_, w| w.idleie().set_bit()) };
     }
 
+    /// Stop listening for idle interrupt event
     pub fn unlisten_idle(&mut self) {
         unsafe { (*USART::ptr()).cr1.modify(|_, w| w.idleie().clear_bit()) };
     }
 
+    /// Returns true if the line idle status is set
     pub fn is_idle(&self) -> bool {
         unsafe { (*USART::ptr()).sr.read().idle().bit_is_set() }
     }
 
+    /// Returns true if the rx register is not empty (and can be read)
     pub fn is_rx_not_empty(&self) -> bool {
         unsafe { (*USART::ptr()).sr.read().rxne().bit_is_set() }
+    }
+
+    /// Clear idle line interrupt flag
+    pub fn clear_idle_interrupt(&self) {
+        unsafe {
+            let _ = (*USART::ptr()).sr.read();
+            let _ = (*USART::ptr()).dr.read();
+        }
     }
 }
 
