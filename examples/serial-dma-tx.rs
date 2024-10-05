ba786fa22e47f783fd54e579237534e13b6b0d06
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2024-10-05 23:03:15 +0300

    clean

--- examples/serial-dma-tx.rs
+++ examples/serial-dma-tx.rs
@@ -10,11 +10,7 @@ use panic_halt as _;
 use cortex_m::asm;
 
 use cortex_m_rt::entry;
-use stm32f1xx_hal::{
-    pac,
-    prelude::*,
-    serial::{Config, Serial},
-};
+use stm32f1xx_hal::{pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
@@ -28,31 +24,28 @@ fn main() -> ! {
     let mut afio = p.AFIO.constrain();
     let channels = p.DMA1.split();
 
-    let mut gpioa = p.GPIOA.split();
-    // let mut gpiob = p.GPIOB.split();
+    let gpioa = p.GPIOA.split();
+    // let gpiob = p.GPIOB.split();
 
     // USART1
-    let tx = gpioa.pa9.into_alternate_push_pull(&mut gpioa.crh);
+    let tx = gpioa.pa9;
     let rx = gpioa.pa10;
 
     // USART1
-    // let tx = gpiob.pb6.into_alternate_push_pull(&mut gpiob.crl);
+    // let tx = gpiob.pb6;
     // let rx = gpiob.pb7;
 
     // USART2
-    // let tx = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+    // let tx = gpioa.pa2;
     // let rx = gpioa.pa3;
 
     // USART3
-    // let tx = gpiob.pb10.into_alternate_push_pull(&mut gpiob.crh);
+    // let tx = gpiob.pb10;
     // let rx = gpiob.pb11;
 
-    let serial = Serial::new(
-        p.USART1,
-        (tx, rx, &mut afio.mapr),
-        Config::default().baudrate(9600.bps()),
-        &clocks,
-    );
+    let serial = p
+        .USART1
+        .serial((tx, rx, &mut afio.mapr), 9600.bps(), &clocks);
 
     let tx = serial.tx.with_dma(channels.4);
 
